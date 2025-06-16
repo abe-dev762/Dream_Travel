a97dd4978b11773ee5c05822a51af5124d785430
Ahmad Zaenal Abidin <ahmadabidin762.dev@gmail.com>
2025-06-16 21:16:32 +0800

    make travel webstie: adding responsive navbar and sidebar;

--- js/script.js
+++ js/script.js
@@ -6,3 +6,34 @@ window.addEventListener("scroll", () => {
         navBarDiv.classList.remove('navbar-cng');
     }
 });
+
+const navbarCollapseDiv = document.getElementById('navbarCollapse');
+const navbarShowBtn = document.getElementById('navbarShowBtn');
+const navbarCloseBtn = document.getElementById('navbarCloseBtn');
+
+//for navigation bar
+navbarShowBtn.addEventListener("click", () => {
+    navbarCollapseDiv.classList.add('navbar-collapse-rmw');
+});
+
+//hide sidebar
+navbarCloseBtn.addEventListener("click", () => {
+    navbarCollapseDiv.classList.remove('navbar-collapse-rmw');
+});
+
+document.addEventListener("click", (e) => {
+    if (e.target.id != "navbarCollapse" && e.target.id != "navbarShowBtn" && e.target.parentElement.id != "navbarShowBtn") {
+        navbarCollapseDiv.classList.remove('navbar-collapse-rmw');
+    }
+});
+
+//stop animation and transition during windows resizing//
+let resizeTimer;
+window.addEventListener('resize', () => {
+    document.body.classList.add('resize-animation-stopper');
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+        document.body.classList.remove('resize-animation-stopper');
+        
+    }, 400);
+});
